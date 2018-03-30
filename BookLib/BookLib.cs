e67a1d16ed761634d1718057221c2c37c011e6a6
MattyAB <matthew.beton@gmail.com>
2018-03-30 17:40:19 +0100

    Added basics of BookLib, soon to start implementing Database connection

--- BookLib/BookLib.cs
+++ BookLib/BookLib.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace BookLib
 {
     public class BookLib
     {
+        SqlConnection Connection = new SqlConnection("Data Source=HORCRUX;Initial Catalog=Books;Integrated Security=True");
+
+        public BookLib()
+        {
+
+        }
+
+        public List<Book> GetScanJob(int depth)
+        {
+
+        }
     }
 }
