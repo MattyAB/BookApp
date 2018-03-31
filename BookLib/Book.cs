f1eb03ff7a6b491f3f9091961f41cd71735b0905
MattyAB <matthew.beton@gmail.com>
2018-03-31 17:52:03 +0100

    Added GetBookDetails code and unit test.

--- BookLib/Book.cs
+++ BookLib/Book.cs
@@ -1,7 +1,45 @@
-﻿namespace BookLib
+﻿using Newtonsoft.Json.Linq;
+using System.Net;
+using System;
+
+namespace BookLib
 {
     public class Book
     {
+        public string ISBN;
+        public string title;
+        public string subtitle;
+        public string author;
+        public string publisher;
+        public DateTime publishDate;
 
+        public Book(string ISBN)
+        {
+            this.ISBN = ISBN;
+        }
+
+        public void GetBookDetails()
+        {
+            string url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + ISBN;
+            string s;
+
+            using (WebClient client = new WebClient())
+            {
+                s = client.DownloadString(url);
+            }
+
+            dynamic d = JObject.Parse(s);
+
+            if(d.totalItems == 0)
+            {
+                throw new Exception("Specified ISBN is not valid.");
+            }
+
+            title = d.items[0].volumeInfo.title;
+            subtitle = d.items[0].volumeInfo.subtitle;
+            author = d.items[0].volumeInfo.authors[0];
+            publisher = d.items[0].volumeInfo.publisher;
+            publishDate = DateTime.Parse(d.items[0].volumeInfo.publishedDate.ToString());
+        }
     }
 }
---
+++ BookLibTests/BookTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BookLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLib.Tests
+{
+    [TestClass()]
+    public class BookTests
+    {
+        [TestMethod()]
+        public void GetBookDetailsTest()
+        {
+            Book book = new Book("9781338099133");
+            book.GetBookDetails();
+
+            Assert.AreEqual("Harry Potter and the Cursed Child - Parts One and Two", book.title);
+            Assert.AreEqual("The Official Script Book of the Original West End Production Special Rehearsal Edition", book.subtitle);
+            Assert.AreEqual("J. K. Rowling", book.author);
+            Assert.AreEqual("Arthur A. Levine Books", book.publisher);
+            Assert.AreEqual(DateTime.Parse("2016-07-31"), book.publishDate);
+        }
+    }
+}
